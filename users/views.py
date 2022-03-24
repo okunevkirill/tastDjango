ec82ea9f4765eab95c990900be61aa348fa1eb1f
ok_kir <clicktmin@gmail.com>
2022-03-24 23:52:13 +0400

    added DRF

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import CustomUser
+from users.serializers import CustomUserModelSerializer
+
+
+class CustomUserViewSet(ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserModelSerializer
